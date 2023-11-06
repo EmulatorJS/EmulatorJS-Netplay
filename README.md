c6c231a8b91e6ab173f6bc59a6c2799247bb9f0c
MotorTruck1221 <tuckerj0606@icloud.com>
2023-11-06 14:42:24 -0700

    Add readme

---
+++ README.md
@@ -0,0 +1,13 @@
+# New Netplay (unfinished)
+
+```bash
+npm i -g pnpm
+```
+
+```bash
+pnpm i
+```
+
+```bash
+pnpm dev
+```
--- dist/index.js
+++ dist/index.js
@@ -5,11 +5,12 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const webrtc_1 = require("./webrtc");
+const path_1 = __importDefault(require("path"));
 const server = (0, express_1.default)();
 (0, webrtc_1.createPeerServer)(server, '/webrtc');
 (0, webrtc_1.servePeerLib)(server);
-server.get('/', (req, res) => {
-    res.send('Hello World <script src="/lib/peerjs/peerjs.min.js"></script>');
+server.get('/:path', (req, res) => {
+    res.sendFile(path_1.default.resolve(path_1.default.join('static', req.params.path)));
 });
 server.listen(3000, () => {
     console.log('Server is running');
